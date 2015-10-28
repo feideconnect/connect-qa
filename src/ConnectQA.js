c45dcfb53ea6a104789782e889496c73371812da
Andreas Åkre Solberg <andreas.solberg@uninett.no>
2015-10-28 10:14:47 +0100

    Fix indenting

--- src/ConnectQA.js
+++ src/ConnectQA.js
@@ -1,4 +1,3 @@
-
 "use strict";
 
 
@@ -10,7 +9,7 @@
 
 var util = require('util');
 var optimist = require('optimist')
-	.usage('Usage: $0 --set [string] ')
+    .usage('Usage: $0 --set [string] ')
     .alias('h', 'help')
     .alias('s', 'set')
     .describe('set', 'Select configuration set, as specified in config.json')
@@ -29,60 +28,62 @@ var extend = require('extend');
 var fs = require('fs');
 
 var ConnectQA = Class.extend({
-	"init": function() {
-		var allconfig = JSON.parse(fs.readFileSync('etc/config.json', 'utf8'));
-		var sconfig = {};
+    "init": function() {
+        var allconfig = JSON.parse(fs.readFileSync('etc/config.json', 'utf8'));
+        var sconfig = {};
 
-		// console.log(argv);
+        // console.log(argv);
 
-		if (argv.help) {
-			console.log(optimist.help());
-			process.exit()
+        if (argv.help) {
+            console.log(optimist.help());
+            process.exit()
 
-		}
+        }
 
-		if (allconfig.common) {
-			extend(sconfig, allconfig.common);
-		}
-		if (argv.set) {
-			if (allconfig.sets[argv.set]) {
-				extend(sconfig, allconfig.sets[argv.set], {"set": argv.set});
-			} else {
-				throw new Error("Could not find config for set [" + argv.set + "]")
-			}
-			
-		} else if (allconfig.default && allconfig.sets[allconfig.default]) {
-			extend(sconfig, allconfig.sets[allconfig.default], {"set": allconfig.default});
-		}
+        if (allconfig.common) {
+            extend(sconfig, allconfig.common);
+        }
+        if (argv.set) {
+            if (allconfig.sets[argv.set]) {
+                extend(sconfig, allconfig.sets[argv.set], {
+                    "set": argv.set
+                });
+            } else {
+                throw new Error("Could not find config for set [" + argv.set + "]")
+            }
 
-		this.sconfig = sconfig;
+        } else if (allconfig.default && allconfig.sets[allconfig.default]) {
+            extend(sconfig, allconfig.sets[allconfig.default], {
+                "set": allconfig.default
+            });
+        }
 
-		this.oce = new OCEngine(sconfig);
-		this.codeflow = new CodeFlow(sconfig);
+        this.sconfig = sconfig;
+
+        this.oce = new OCEngine(sconfig);
+        this.codeflow = new CodeFlow(sconfig);
 
 
-	},
-	"run": function() {
-		var that = this;
+    },
+    "run": function() {
+        var that = this;
 
-		return Promise.resolve()
-			.then(function() {
-				return that.oce.run();
-			})
-			.then(function() {
-				return that.codeflow.run();
-			})
-			.then(function(token) {
-				var api = new ConnectAPI(that.sconfig, token);
-				return api.run();
-			})
-			.then(function() {
-				console.log(" ---- done ----");
-			});
+        return Promise.resolve()
+            .then(function() {
+                return that.oce.run();
+            })
+            .then(function() {
+                return that.codeflow.run();
+            })
+            .then(function(token) {
+                var api = new ConnectAPI(that.sconfig, token);
+                return api.run();
+            })
+            .then(function() {
+                console.log(" ---- done ----");
+            });
 
-	}
+    }
 })
 
 exports.ConnectQA = ConnectQA;
-
-
