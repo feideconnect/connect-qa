a1dd32a2251725fe5d58c7d300e826b59b37cadb
Andreas Åkre Solberg <andreas.solberg@uninett.no>
2015-10-28 10:11:15 +0100

    Use [open] to open browser with temporary html file for debugging

--- src/Engine/Response.js
+++ src/Engine/Response.js
@@ -5,6 +5,7 @@ var chai = require('chai');
 var tmp = require('tmp');
 var fs = require('fs');
 var readlineSync = require('readline-sync');
+var open = require("open");
 
 var Logger = require('../Logger').Logger;
 var Class = require('../Class').Class;
@@ -25,20 +26,24 @@ var Response = Class.extend({
     "debug": function() {
         var that = this;
 
+        that.log.info("Debugging [" + this.type + "]");
+
         console.log(" - - - - - - - - - - - - - - - - -  Body [" + this.type + "] - - - - - - - - -- - - - - - - - -");
         console.log(this.body);
         console.log(" - - - - -- - - - -- - - - -- - - - -- - - - -- - - - -- - - - -- - - - -- - - - -  /Body - - - - -- - - - -- - - - -- - - - -");
 
-        var tmpobj = tmp.fileSync();
 
-        console.log("File: ", tmpobj.name);
-        console.log("Filedescriptor: ", tmpobj.fd);
+
+        var tmpobj = tmp.fileSync({prefix: 'connectqa-', postfix: '.html', keep: true });
 
         fs.writeFileSync(tmpobj.name, this.body);
         that.log.info('open -a Google\\ Chrome  ' + tmpobj.name);
+        open(tmpobj.name, 'Google Chrome');
 
-        that.log.info("Hit [enter] to continue");
-        var x = readlineSync.question('Hit [enter] to continue.');
+
+
+        // that.log.info("Hit [enter] to continue");
+        // var x = readlineSync.question('Hit [enter] to continue.');
 
     }
 });
