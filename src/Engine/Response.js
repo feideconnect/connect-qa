9072c30f1e9df70c4c1e5c988289dc4600bf91bd
Andreas Åkre Solberg <andreas.solberg@uninett.no>
2015-10-28 09:03:27 +0100

    Reponse debug() will now write HTML to temporary file, and hint about how to open it in a browser.

--- src/Engine/Response.js
+++ src/Engine/Response.js
@@ -1,29 +1,46 @@
 
 "use strict";
 
+var chai = require('chai');
+var tmp = require('tmp');
+var fs = require('fs');
+var readlineSync = require('readline-sync');
+
 var Logger = require('../Logger').Logger;
 var Class = require('../Class').Class;
-var chai = require('chai');
+
 
 var Response = Class.extend({
-	"init": function(response, body, c) {
+    "init": function(response, body, c) {
 
-		this.log = Logger.get();
-		this.assert = chai.assert;
+        this.log = Logger.get();
+        this.assert = chai.assert;
 
-		this.response = response;
-		this.body = body;
-		this.$ = c; 
+        this.response = response;
+        this.body = body;
+        this.$ = c; 
 
-		this.type = "Response";
-	},
-	"debug": function() {
+        this.type = "Response";
+    },
+    "debug": function() {
+        var that = this;
 
-		console.log(" - - - - - - - - - - - - - - - - -  Body [" + this.type + "] - - - - - - - - -- - - - - - - - -");
-		console.log(this.body);
-		console.log(" - - - - -- - - - -- - - - -- - - - -- - - - -- - - - -- - - - -- - - - -- - - - -  /Body - - - - -- - - - -- - - - -- - - - -");
+        console.log(" - - - - - - - - - - - - - - - - -  Body [" + this.type + "] - - - - - - - - -- - - - - - - - -");
+        console.log(this.body);
+        console.log(" - - - - -- - - - -- - - - -- - - - -- - - - -- - - - -- - - - -- - - - -- - - - -  /Body - - - - -- - - - -- - - - -- - - - -");
 
-	}
+        var tmpobj = tmp.fileSync();
+
+        console.log("File: ", tmpobj.name);
+        console.log("Filedescriptor: ", tmpobj.fd);
+
+        fs.writeFileSync(tmpobj.name, this.body);
+        that.log.info('open -a Google\\ Chrome  ' + tmpobj.name);
+
+        that.log.info("Hit [enter] to continue");
+        var x = readlineSync.question('Hit [enter] to continue.');
+
+    }
 });
 
 
