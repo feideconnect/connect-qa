27491af29b4445a36828221d4799b1126675c6f5
Andreas Åkre Solberg <andreas.solberg@uninett.no>
2016-02-24 14:10:29 +0100

    Add slack notification

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 #!/usr/bin/env node
+"use strict";
 
 var ConnectQA = require('./src/ConnectQA').ConnectQA;
+var Slack = require('node-slackr');
+
+
+var slackkey = process.env.SLACK;
 
 var c = new ConnectQA();
-c.run(); 
+var slack = new Slack(slackkey,{
+  channel: "#dataporten",
+  username: "dataporten-integration-tst",
+  // icon_url: "http://domain.com/image.png",
+  icon_emoji: ":ghost:"
+});
+
+
+c.run()
+	.catch(function(error) {
+		// console.error("ERROR", error);
+		slack.notify("Error running Dataporten test: " +  error)
+	})
