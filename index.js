e56f5483a237fb6a70f8ded2e0303392f73f0203
Andreas Åkre Solberg <andreas.solberg@uninett.no>
2016-02-24 14:15:45 +0100

    Avoid slack if not configured

--- index.js
+++ index.js
@@ -8,16 +8,26 @@ var Slack = require('node-slackr');
 var slackkey = process.env.SLACK;
 
 var c = new ConnectQA();
-var slack = new Slack(slackkey,{
-  channel: "#dataporten",
-  username: "dataporten-integration-tst",
-  // icon_url: "http://domain.com/image.png",
-  icon_emoji: ":ghost:"
-});
+
+var slack = null;
+if  (slackkey) {
+	slack = new Slack(slackkey,{
+	  channel: "#dataporten",
+	  username: "dataporten-integration-tst",
+	  // icon_url: "http://domain.com/image.png",
+	  icon_emoji: ":ghost:"
+	});
+}
+
+
 
 
 c.run()
 	.catch(function(error) {
 		// console.error("ERROR", error);
-		slack.notify("Error running Dataporten test: " +  error)
+		// 
+		if (slack !== null) {
+			slack.notify("Error running Dataporten test: " +  error)
+		}
+		
 	})
